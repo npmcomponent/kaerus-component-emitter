a57bb4512dc016b385c983e97591eea2e2ac8571
anders elo <anders@kaerus.com>
2013-04-11 16:52:53 +0200

    simplified

--- index.js
+++ index.js
@@ -21,9 +21,8 @@ Emitter.prototype.listeners = function(event) {
     var handlers = this._events[event];
 
     if(!handlers) return [];
-    if(!Array.isArray(handlers)) return [handlers];
     
-    return handlers;
+    return handlers.filter(function(f){return f !==before && f !==after});
 }
 
 Emitter.prototype.hasListeners = function(event) {
@@ -36,37 +35,21 @@ function after(){};
 Emitter.prototype.on = function(event,handler,first) {
     var events = this._events[event];
 
-    if(!events) {
-        this._events[event] = handler;
-    } else if(!Array.isArray(events)) {
-        if(events !== handler) {
-            if(first === undefined) this._events[event] = [events,handler];
-            else if(first) this._events[event] = [handler,before,events];
-            else this._events[event] = [events,after,handler];
-        }  
-    } else {
-        if(events.indexOf(handler) < 0) {
-            if(first === undefined) events.splice(events.indexOf(after),0,handler);
-            else if(first) {
-                if((events.indexOf(before))<0) events.splice(0,0,before);
-                events.splice(events.indexOf(before),0,handler);
-            }    
-            else {
-                if(events.indexOf(after)<0) events[events.length] = after;
-                events.splice(events.indexOf(after)+1,0,handler);
-            }    
-        } 
-    }    
+    if(!events) events = this._events[event] = [before,after];
+
+    if(first === undefined) events.splice(events.indexOf(after),0,handler);
+    else if(first === true) events.splice(events.indexOf(before),0,handler);
+    else events[events.length] = handler;     
 
     return this;
 }
 
 Emitter.prototype.before = function(event,handler) {
-     return this.on(event,handler,true);
+    return this.on(event,handler,true);
 }
 
 Emitter.prototype.after = function(event,handler) {
-     return this.on(event,handler,false);
+    return this.on(event,handler,false);
 }
 
 Emitter.prototype.off = function(event,handler) {
@@ -82,41 +65,27 @@ Emitter.prototype.off = function(event,handler) {
 
     if(!handler) {
         delete this._events[event];
-    }
-    else if(!Array.isArray(events)){
-        if((events._of || events) === handler)
-            delete this._events[event];    
     } else {
         this._events[event] = this._events[event].filter(function(f) {
             return (f._of || f) !== handler;
         });
-        /* undefines event when no handler is attached */
-        /* or unwraps handler array on single handler. */
-        var length = this._events[event].length; 
-        
-        if(!length) 
-            delete this._events[event];
-        else if(length === 1) 
-            this._events[event] = this._events[event][0];
     } 
 
     return this;
 }
 
 Emitter.prototype.emit = function(event) {
-    var events = this._events[event];
+    var events = this._events[event], handler;
 
     if(!events) return this;   
 
     var args = Array.prototype.slice.call(arguments,1);
 
-    if(!Array.isArray(events)) {
-        events.apply(this,args);
-    } else {
-        for(var i = 0, l = events.length; i < l; i++){
-            if(events[i].apply(this,args) === false) break;
-        }        
-    }
+    for(var i = 0, l = events.length; i < l; i++){
+        handler = events[i];
+        if(handler !== before && handler !== after && 
+            handler.apply(this,args) === false) break;
+    }        
 
     return this;
 }
